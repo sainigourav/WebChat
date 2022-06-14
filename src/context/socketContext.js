3ee4511bdbfc8677ebfcf4a7fe753fcf9ed69ad0
Gourav Saini <gourav.saini@meritech.co.in>
2022-06-14 14:46:39 +0530

    second commit

--- src/context/socketContext.js
+++ src/context/socketContext.js
@@ -2,9 +2,7 @@ import { createContext, useContext } from "react";
 import io from "socket.io-client";
 import { Socket_URL } from "../utils/config";
 
-const SOCKET_URL = Socket_URL;
-
-const socket = io.connect(SOCKET_URL);
+const socket = io.connect(Socket_URL);
 
 const SocketContext = createContext();
 
