793bc05b3b71dbb3eaa9154b6ca41e69a2df7a77
Gourav Saini <gourav.saini@meritech.co.in>
2022-06-14 14:14:04 +0530

    second commit

--- src/context/socketContext.js
+++ src/context/socketContext.js
@@ -1,9 +1,8 @@
 import { createContext, useContext } from "react";
 import io from "socket.io-client";
+import { Socket_URL } from "../utils/config";
 
-const SOCKET_URL = window.location.origin.includes("localhost")
-	? "http://localhost:5000"
-	: "https://whatsapp-web-clone-backend.herokuapp.com/";
+const SOCKET_URL = Socket_URL;
 
 const socket = io.connect(SOCKET_URL);
 
--- src/utils/config.js
+++ src/utils/config.js
@@ -19,5 +19,6 @@ const config = () => {
 const appConfig = config();
 export const ImageAPI = appConfig.REACT_APP_IMAGE_URL;
 export const React_App_Url = appConfig.REACT_APP_PUBLIC_URL;
+export const Socket_URL = appConfig.REACT_APP_SOCKET_URL;
 export const API_URL = appConfig.REACT_APP_API_URL;
   
